de408123160423c4633dead9f03d761f4fe8d712
Oscarwin <ltengpeter@gmail.com>
2019-09-05 08:59:32 +0800

    commit

---
+++ point-to-offer/Python/字符串的排列.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+# 题目描述
+# 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+# 输入描述:
+# 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+
+class Solution:
+    def Permutation(self, ss):
+        result = []
+        ss_len = len(ss)
+
+        if ss_len == 0:
+            return result
+        
+        if ss_len == 1:
+            result.append(ss)
+            return result
+        
+        
+        for i in range(ss_len):
+
+
+
+
